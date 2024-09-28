a69e0f0310371331b131450e7e29d1da94409077
Habibur Rahman <habibur88454@gmail.com>
2024-09-29 00:15:04 +0600

    js data type

---
+++ types.js
@@ -0,0 +1,18 @@
+//  primitive type
+// number string boolean null undefined
+
+// non-primitive
+// array object
+
+let a = 6;
+let b = 3;
+console.log(a, b)
+b = 10
+console.log(a, b)
+
+let user1 = { position: 'developer' }
+let user2 = user1
+user2.position = 'kamla' // partial change
+user2 = {position: 'engineer'}
+console.log(user1, user2)
+
